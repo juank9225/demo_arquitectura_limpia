926d0d51ae0109e4759c9125e390b09e9bb2b1b1
juan carlos salcedo <juank9225@gmail.com>
2021-07-15 01:05:58 -0500

    Se crea las colecciones del dominio como answer y question

--- src/main/java/com/example/proyectomodular/proyecto/modular/answer/domain/Answer.java
+++ src/main/java/com/example/proyectomodular/proyecto/modular/answer/domain/Answer.java
@@ -1,4 +1,72 @@
-package com.example.proyectomodular.proyecto.modular.answer.domain.repository;
+package com.example.proyectomodular.proyecto.modular.answer.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class Answer {
+    @Id
+    private String id;
+    private String userId;
+    private String questionId;
+    private String answer;
+    private Integer vote;
+    private Boolean modificada;
+    private Integer vecesModificada;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(String questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public Integer getVote() {
+        return vote;
+    }
+
+    public void setVote(Integer vote) {
+        this.vote = vote;
+    }
+
+    public Boolean getModificada() {
+        return modificada;
+    }
+
+    public void setModificada(Boolean modificada) {
+        this.modificada = modificada;
+    }
+
+    public Integer getVecesModificada() {
+        return vecesModificada;
+    }
+
+    public void setVecesModificada(Integer vecesModificada) {
+        this.vecesModificada = vecesModificada;
+    }
 }
